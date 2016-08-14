3bd59afae281ce7e18b0dbe8743848e9d1a9ee1b
mars0i <mabrams@uab.edu>
2016-08-14 18:38:42 -0500

    revised scope of this-as, added many experiments

--- src/ags1/core.cljs
+++ src/ags1/core.cljs
@@ -11,32 +11,42 @@
 
 (defn on-js-reload [])
 
-(defn run-sim []
-  (this-as that
-    (let [abm (.-ABM that)
-          util (.-Util abm)
-          model (.-Model abm)
-          prototype (.-prototype model)]
-      ;(println (.keys js/Object (.-ABM that))) ; DEBUG
-      ;(println (.keys js/Object abm)) ; DEBUG
+(def abm (this-as that (.-ABM that)))
+(def util (.-Util abm))
+(def model (.-Model abm))
+(def prototype (.-prototype model))
 
-      ;; STARTUP: leave as default
-      ;; SETUP:
-      (set! (.-setup prototype)
-            (fn []
-              (println "setup")))
-      ;; STEP:
-      (set! (.-step prototype)  ; STEP
-            (fn []
-              (println "step")))
-      (let [sim (model (clj->js {:div "layers"  ; sim is called 'model' in jsmodel.html
-                                 :size 13
-                                 :minX -16
-                                 :maxX 16
-                                 :minY -16
-                                 :maxY 16
-                                 :isTorus true}))]
-        (.debug sim)
-        (.start sim)))))
+;; STARTUP: leave as default
 
-(run-sim)
+;; SETUP:
+(set! (.-setup prototype)
+      (fn []
+        (println "setup")))
+
+;; STEP:
+(set! (.-step prototype)  ; STEP
+      (fn []
+        (println "step")))
+
+(def sim-params (clj->js {:div "layers"
+                          :size 13
+                          :minX -16
+                          :maxX 16
+                          :minY -16
+                          :maxY 16
+                          :isTorus true}))
+
+;(def sim (abm/Model sim-params)) ; doesn't run
+;(def sim (ABM/Model sim-params)) ; doesn't run
+;(def sim (model abm sim-params)) ; called this way, it complains because params it gets is ABM
+;(def sim ((.Model abm) sim-params)) ; runs but complains because params it gets is ABM
+;(def sim (.Model js/ABM sim-params)) ; runs, but 'this' points to ABM, which doesn't have prototype's functions
+;(this-as this (def sim (.Model sim-params))) ; runs, but 'this' points to sim-params
+;(this-as this (def sim (.Model this sim-params))) ; runs, but 'this' points to global which doesn't have prototype's functions
+;(def sim (model model sim-params)) ; runs, but complains because params it gets is model
+;(def sim (model js/this sim-params)) ; runs, but "this$" is undefined
+;(this-as this (def sim (.Model this abm sim-params))) 
+(def sim (model sim-params)) ; runs, but 'this' points to index.html, which doesn't have prototype's functions
+
+(.debug sim)
+(.start sim)
