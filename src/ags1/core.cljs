205225c2ea7307d5741c894a0eae5aad0d77af59
mars0i <mabrams@uab.edu>
2016-08-17 11:21:22 -0500

    removed 500 tick timeout code, made more like jsmodel.html

--- src/ags1/core.cljs
+++ src/ags1/core.cljs
@@ -1,45 +1,43 @@
 ;; Marshall Abrams
-;; port of jsmodel.html from the agentscript site to Clojurescript.
+;; Clojurescript port of jsmodel.html from the Agentscript site
 ;; Copyright 2016 Marshall Abrams. Released under GPL 3.0.
 
-(ns ags1.core
-  (:require ))
+(ns ags1.core)
 
-(enable-console-print!)
+(enable-console-print!) ; makes (println ...) calls like console.log(...) calls
 
-;(def max-ticks 500) ; runs this many ticks
-(def max-ticks nil) ; runs forever
-
-;; Parameters that will be passed to the model (sim, this-inst below):
+;; Parameters that will be passed to the model (i.e. sim or this-inst below):
 (def sim-params (clj->js {:div "layers"
-                          :size 13
+                          :size 13  ; scale relative to browser
                           :minX -16 ; number of patches left of center patch
                           :maxX 16  ; i.e. actual width is now 33
-                          :minY -16 ; patches below (?) center
+                          :minY -16 ; patches below (?) center patch
                           :maxY 16  ; i.e. height is also 33
                           :isTorus true}))
 
-;; Uses this = top-level window:
-(def abm (this-as this-top (.-ABM this-top)))   
-
+;; convenience defs for use below
+(def abm (this-as this-top (.-ABM this-top))) ; uses this = top-level window
 (def util (.-Util abm))
-(def model (.-Model abm)) ; lowercase clearer to distinguish our var from the accessor
+(def model (.-Model abm))
+(def prototype (.-prototype model))
 
-; STARTUP: leave default
+;; startup runs when an instance of Model is created
+(set! (.-startup prototype) (fn [] (println "startup")))
 
-; SETUP:
-(set! (.-setup (.-prototype (.-Model abm)))
+;; setup runs when an instance of Model is created, and during reset
+(set! (.-setup prototype)
       (fn []
+        (println "setup")
         (this-as this-inst   ; this: the current instance of Model
           (let [turtles (.-turtles this-inst)
                 patches (.-patches this-inst)]
 
             (set! (.-refreshPatches this-inst) false) ; not updating patches
             (set! (.-refreshLinks this-inst) false)   ; no links to update
-            (.setUseSprites (.-turtles this-inst))    ; use faster bitmap turtle images
+            (.setUseSprites turtles)                  ; faster bitmap turtles
             (set! (.-population this-inst) 100)       ; how many turtles?
             (set! (.-speed this-inst) 0.5)            ; how fast do they go?
-            (set! (.-wiggle this-inst) (.degToRad util 30))
+            (set! (.-wiggle this-inst) (.degToRad util 30)) ; random turn param
 
             (doseq [p patches]
               (set! (.-color p) (.randomGray util)))
@@ -47,33 +45,33 @@
             (.create turtles (.-population this-inst))
 
             (doseq [t turtles]
-              (let [pt (js->clj (.randomPt (.-patches this-inst)))]
+              (let [pt (js->clj (.randomPt patches))]
                 (.setXY t (first pt) (second pt))))
 
             (println "patches; "  (count patches) " turtles: " (count turtles))))))
 
-; STEP:
-(set! (.-step (.-prototype (.-Model abm)))
+;; step is what runs on every tick
+(set! (.-step prototype)
       (fn []
         (this-as this-inst   ; this: the current instance of Model
-           (when (and max-ticks (> (.-ticks (.-anim this-inst)) max-ticks))
-             (.stop this-inst))
+           ;(when (== 0 (mod (.-ticks (.-anim this-inst)) 100))
+           ;  (println (.toString (.-anim this-inst))))
            (doseq [t (.-turtles this-inst)]
-                   (.rotate t (.randomCentered util (.-wiggle this-inst)))
-                   (.forward t (.-speed this-inst))))))
+             (.rotate t (.randomCentered util (.-wiggle this-inst)))
+             (.forward t (.-speed this-inst))))))
+
 
 ;; Create the model:
 (defonce sim (new model sim-params)) ; '(model. sim-params)' works, too
-;(.debug sim) ; print info to console, put model vars in global name ;space, make a turtle corral over on the right
-(.start sim) ; Run the model!  (.reset sim true) works here, too
+;(.debug sim) ; print to console, model vars in global, turtle "sprite sheet" on right
+(.start sim) ; Run it!  (.reset sim true) works here, too
 
-;; Runs when figwheel reloads the compiled version of this source:
+;; Runs when figwheel reloads the code:
 (defn on-js-reload []
-  (.reset sim true))  ; reset and restart the model. (.start ...) above is redundant on reload (?)
+  (.reset sim true))
+;; resets and restarts the model. (.start ...) above is redundant on reload (?)
 
-;; Above we use defonce rather than def, and the call to reset in on-js-reload 
-;; in order to work well with figwheel, which is an add-on to Leiningen, a 
-;; standard build tool for Clojure and Clojurescript.  Figwheel reloads the 
-;; code into the browser when source files are touched.  Without these tweaks 
-;; there will be multiple instances of the model running at once and/or things
-;; will get confused in the browser in other ways.
+;; Note: Above we use defonce rather than def, and the call to reset in 
+;; on-js-reload in order to work with figwheel.  Otherwise there 
+;; would be will be multiple instances of the model running at once 
+;; and/or things would get confused in the browser in other ways.
