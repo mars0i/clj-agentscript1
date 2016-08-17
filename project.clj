5b008b34a7533d5b7f10f70c976321a40ecfea79
mars0i <mabrams@uab.edu>
2016-08-17 00:21:04 -0500

    updated to more recent version of Clojurescript

--- project.clj
+++ project.clj
@@ -7,8 +7,8 @@
   :min-lein-version "2.6.1"
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.89"]
-                 ;[org.clojure/clojurescript "1.9.198"]
+                 ;[org.clojure/clojurescript "1.9.89"]
+                 [org.clojure/clojurescript "1.9.211"]
                  [org.clojure/core.async "0.2.385"
                   :exclusions [org.clojure/tools.reader]]]
 
